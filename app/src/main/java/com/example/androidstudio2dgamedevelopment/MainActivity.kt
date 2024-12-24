f3db4ea27680d3f0fa4e51b9a059b1bb1ae031b8
lschacon <lschacon7@gmail.com>
2024-12-24 01:32:48 -0500

    Setup

---
+++ app/src/main/java/com/example/androidstudio2dgamedevelopment/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.androidstudio2dgamedevelopment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.androidstudio2dgamedevelopment.ui.theme.AndroidStudio2DGameDevelopmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidStudio2DGameDevelopmentTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidStudio2DGameDevelopmentTheme {
+        Greeting("Android")
+    }
+}
